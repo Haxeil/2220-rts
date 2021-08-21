ba2e8b36298f92dcf91ab4c245713403c4d44f2b
shanks-red <lezharayman@gmail.com>
2021-08-21 18:40:53 +0100

    added state maching logic

--- scenes/Units/Unit.cs
+++ scenes/Units/Unit.cs
@@ -11,6 +11,7 @@ public abstract class Unit : KinematicBody
 	// Determines who controls this unit, and who this unit chooses to engage.
 	[Export] public Faction _Faction;
 
+
 	private UnitGroup unit_group = null; // The group of the unit, null otherwise
 	public UnitGroup UnitGroup
 	{
@@ -37,6 +38,9 @@ public abstract class Unit : KinematicBody
 			Nav = GetNode<Navigation>(value);
 		}
 	}
+	// Called When the Unit State changes.
+	[Signal] protected delegate void StateChanged(Enums.UnitState s);
+	public UnitState state = new UnitState();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -51,5 +55,26 @@ public abstract class Unit : KinematicBody
 	public abstract void Select();
 	// Called when the player has deselected this Unit.
 	public abstract void Deselect();
+	// Called when the state changes to play an animation.
+	protected abstract void PlayAnimation();
+
+	// Called when a unit state changes;
+	public void _On_UnitStateChanged(Enums.UnitState s)
+	{
+		// if the current gameplay state is DEAD dont change the state.
+		if (state.currentState == Enums.UnitState.DEAD) {return;}
+
+
+		state.nextState = s; //IDLE // MOVE // ATTACK // DIE 
+
+		// dont change the prevState if the nextState = the currentState
+		if (state.nextState != state.currentState)
+		{
+			state.prevState = state.currentState; 
+		}
+		state.currentState = state.nextState; 
+		PlayAnimation();
+
+	}
 }
 
