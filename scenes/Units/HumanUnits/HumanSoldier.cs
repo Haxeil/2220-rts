35b11f5e23d5cf8fe7c2efb62ec3da6b306013b6
shanks-red <lezharayman@gmail.com>
2021-08-21 18:42:24 +0100

    animation now is handled by the state machine

--- scenes/Units/HumanUnits/HumanSoldier.cs
+++ scenes/Units/HumanUnits/HumanSoldier.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
+
 public class HumanSoldier : Unit
 {
 	[Export] private Vector3[] path;
@@ -8,13 +9,15 @@ public class HumanSoldier : Unit
 	[Export] private float speed = 12f;
 	//private Navigation nav;
 	private Spatial character;
+	private AnimationPlayer animPlayer;
 
 	[Export] private float angular_velocity = 7f;
+
 	public override void _Ready()
 	{
 		base._Ready(); // Call Unit._Ready() to get the Nav
 		character = GetNode<Spatial>("AnimatedCharacter");
-		
+		animPlayer = character.GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	//called from Cam script 
@@ -28,7 +31,6 @@ public class HumanSoldier : Unit
 		
 		
 		if (path == null) {
-			
 			return;
 		}
 
@@ -36,11 +38,12 @@ public class HumanSoldier : Unit
 			var direction = (path[pathIndx] - this.GlobalTransform.origin);
 			if (direction.Length() < 0.1) {
 				pathIndx += 1;
+				EmitSignal("StateChanged", Enums.UnitState.IDLE);
 			} else {
 				
 				var velocity = this.MoveAndSlide(direction.Normalized() * speed, Vector3.Up, false, 4, Mathf.Pi/4, false);
-
-				character.GetNode<AnimationPlayer>("AnimationPlayer").Play("Walking");
+				// Emit the signal to change the state
+				EmitSignal("StateChanged", Enums.UnitState.MOVE);
 				// change in y angle 
 				var deltaYAngle = Mathf.LerpAngle(Rotation.y, Mathf.Atan2(velocity.x, velocity.z), delta * angular_velocity);
 
@@ -59,7 +62,28 @@ public class HumanSoldier : Unit
 	public override void Deselect() {
 		GetNode<MeshInstance>("Selected").Hide();
 	}
+
+
+	protected override void PlayAnimation()
+	{
+
+		switch (state.currentState)
+		{
+			case Enums.UnitState.IDLE:
+				animPlayer.Play("IDLE");
+				break;
+			case Enums.UnitState.MOVE:
+				animPlayer.Play("MOVE");
+				break;
+			case Enums.UnitState.ATTACK:
+				animPlayer.Play("ATTACK");
+				break;
+			case Enums.UnitState.RELOAD:
+				animPlayer.Play("RELOAD");
+				break;
+			case Enums.UnitState.DEAD:
+				animPlayer.Play("DEAD");
+				break;
+		}
+	}
 }
-
-
-
