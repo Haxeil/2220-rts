5aa91dd884f5374716fa1dd910d25628803b31b9
shanks-red <lezharayman@gmail.com>
2021-08-25 01:09:55 +0100

    UI can display UnitsInfo and inform buildings to produce units

---
+++ UI/MainPanel.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+public class MainPanel : Control
+{
+
+	private TextureRect BoxSelection;
+	private int tableIndx = 0;
+	
+
+	public override void _Ready()
+	{
+		BoxSelection = GetNode<TextureRect>("BoxSelection");
+	}
+	// Called From Buildings, when they are selected.
+	public void DisplayTextures(Godot.Collections.Dictionary<Enums.Units, Godot.Collections.Array<Texture>> unitData, String groupName) {
+		foreach (var uD in unitData) {
+			for (var idx = 0; idx < BoxSelection.GetChildCount(); idx++) {
+				var btn = BoxSelection.GetChild<Btn>(idx);
+				// when the btn has no data yet.
+				if (btn.unit == Enums.Units.None) {
+					btn.unit = uD.Key;
+					btn.building = groupName;
+					btn.TextureNormal = uD.Value[0];
+					btn.TexturePressed = uD.Value[1];
+					btn.TextureHover = uD.Value[2];
+					btn.TextureDisabled = uD.Value[3];
+				}
+			}
+		}
+
+	}
+
+
+
+
+
+}
+
+
